9ea40329f9ebca728343ddb0bc4fb0031fd6426a
nibs404 <niilo02keskinen@gmail.com>
2024-11-21 10:35:16 +0200

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
-import logo from './assets/ailimelogo.png';
+import logo from './assets/ailimelogo.png'
+//  import './Blox.jsx'
+
 
 function App() {
-  
+
   return (
     <>
       <header>
-        
-        <h1><img src={logo} alt="logo" id='limelogo' />Luentomuistiinpanosofta</h1>
+        <div className="titlecont">
+        <h1>
+          <img src={logo} alt="logo" id='limelogo' />
+          Luentomuistiinpanosofta
+        </h1>
+        </div>
+
         <textarea name="muistiinpano_kenttä" id="note"></textarea>
         <button id='addbtn'>Save</button>
         </header>    
+
+        {/* <div>
+          <Blox />
+        </div> */}
+        
     </>
+
   )
 }
 
